4673618a2c62dd3e218c56e199ca23276f940279
ueki5 <ueki005@gmail.com>
2026-08-03 10:21:37 +0900

    do記法に戻す。mtlを追加

--- hs007/app/Main.hs
+++ hs007/app/Main.hs
@@ -3,23 +3,24 @@ module Main where
 import Stack
 
 program :: StackM (Maybe Int, Maybe Int)
--- program = do
---   push 1
---   push 2
---   push 3
---   a <- pop
---   b <- pop
---   push 100
---   pure (a, b)
-program =
+program = do
   push 1
-    >> push 2
-    >> push 3
-    >> pop
-    >>= \a ->
-      pop >>= \b ->
-        push 100
-          >> pure (a, b)
+  push 2
+  push 3
+  a <- pop
+  b <- pop
+  push 100
+  pure (a, b)
+
+-- program =
+--   push 1
+--     >> push 2
+--     >> push 3
+--     >> pop
+--     >>= \a ->
+--       pop >>= \b ->
+--         push 100
+--           >> pure (a, b)
 
 main :: IO ()
 main = do
